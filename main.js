395b7590788c76f1c89d8bd5e2fcc59d625c5e42
Raudel Garcia Rodriguez <Raudel@MacBook-Air-de-Raudel.local>
2017-04-27 13:44:12 -0400

    Portfolio

---
+++ main.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+	setBandings();
+  $('#contact_form').bootstrapValidator({
+        
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            first_name: {
+                validators: {
+                        stringLength: {
+                        min: 2,
+                    },
+                        notEmpty: {
+                        message: 'Please supply your first name and your last name'
+                    }
+                }
+            },
+             
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply your email address'
+                    },
+                    emailAddress: {
+                        message: 'Please supply a valid email address'
+                    }
+                }
+            },
+
+            comment: {
+                validators: {
+                      stringLength: {
+                        min: 10,
+                        max: 200,
+                        message:'Please enter at least 10 characters and no more than 200'
+                    },
+                    notEmpty: {
+                        message: 'No comment'
+                    }
+                    }
+                }
+            }
+        })
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
+  
+});
+
+function setBandings() {
+	$("nav a").click(function(e){
+		e.preventDefault();
+		var sectionID = e.currentTarget.id + "Section";
+
+		$("html body").animate({
+			scrollTop: $("#" + sectionID).offset().top
+		}, 1200)
+	})
+}
